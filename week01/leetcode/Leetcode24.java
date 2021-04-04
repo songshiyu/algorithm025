b9a615034337256b7a70537aa842e73f08761995
songshiyu <songshiyu_s@163.com>
2021-04-04 12:02:52 +0800

    调整代码结构

---
+++ week01/leetcode/Leetcode24.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+/**
+ * @author songshiyu
+ * @date 2021/4/2 23:04
+ **/
+public class Leetcode24 {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode swapPairs(ListNode head) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while (temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+}
