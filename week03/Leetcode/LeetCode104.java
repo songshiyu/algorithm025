988f196e37ff516e478048f0168833ad1fe0249c
songshiyu <songshiyu_s@163.com>
2021-04-12 22:32:44 +0800

    leetcode104

---
+++ week03/Leetcode/LeetCode104.java
@@ -0,0 +1,37 @@
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * @author songshiyu 二叉树的最大深度
+ * @date 2021/4/12 22:19
+ **/
+public class LeetCode104 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
