9af754c9d63398cb2ab078dd9cae9925babc2637
ssy3 <ssy3@meitu.com>
2021-04-16 09:55:27 +0800

    homework-leetcode77

--- week03/Leetcode/LeetCode297.java
+++ week03/Leetcode/LeetCode297.java
@@ -1,6 +1,29 @@
 /**
- * @author songshiyu
+ * @author songshiyu  二叉树的序列化与反序列化
  * @date 2021/4/13 20:07
  **/
 public class LeetCode297 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    // Encodes a tree to a single string.
+    //进行二叉树的层序遍历
+    public String serialize(TreeNode root) {
+        return null;
+    }
+
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+
+        return null;
+    }
 }
